952ec4553c9d3606b7540a64432a6c7a7e591ded
abejide2 <abejide2@yahoo.com>
2023-06-29 11:44:55 -0500

    Recent addition

--- testscript/script.sh
+++ testscript/script.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "This is a very good practice $1"
+echo "This is a very good practice by  Police $1", "the  cops came right on time before the thief could escape $2"."The whole village woke up to see MR Joe's shop burgled  by a group of thieves $3".
+"The were all very happy that the thieves were caught and taken to the  prison $4"
+"Everybody was shocked to see that the leader of the gang was the son of the village head, the man has brought a great shame to the family $5"
